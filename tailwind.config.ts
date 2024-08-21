7e2e2c73c7e3a74d7c5ff8f7d6da02d04b2eea05
Thiago Jacques <thiagolinhaes9@gmail.com>
2024-08-21 17:39:05 -0300

    adicionado estilos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,52 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+          200: "rgba(17, 25, 40, 0.75)",
+          300: "rgba(255, 255, 255, 0.125)",
+        },
+        white: {
+          DEFAULT: "#FFF",
+          100: "#BEC1DD",
+          200: "#C1C2D3",
+        },
+        blue: {
+          "100": "#F3FBFE",
+        },
+        color: {
+          1: "#F3FBFE",
+          2: "#34302B",
+          3: "#35312C",
+          4: "#EDF5F7",
+          5: "#36312C",
+          6: "#2B201A",
+        },
+      },
+      scroll: {
+        to: {
+          transform: "translate(calc(-50% - 0.5rem))",
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;

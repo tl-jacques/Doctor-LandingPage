67e6843ed9afa84bdcae257188eac8d4227437e4
Thiago Jacques <thiagolinhaes9@gmail.com>
2024-08-24 09:39:19 -0300

    adicionado `constants` de texto

---
+++ src/constants/index.js
@@ -0,0 +1,95 @@
+export const mainComplains = [
+  {
+    title: "Acne",
+    text: "Presença de espinhas, cravos e inflamações na pele, especialmente no rosto, que afetam a estética e a autoestima",
+  },
+  {
+    title: "Melasma",
+    text: " Manchas escuras na pele, geralmente no rosto, que causam preocupação com a aparência",
+  },
+  {
+    title: "Psoríase",
+    text: "Placas escamosas, vermelhidão e coceira na pele, que podem ser desconfortáveis e visivelmente preocupantes.",
+  },
+  {
+    title: "Dermatite Atópica",
+    text: "Coceira intensa, ressecamento e inflamação da pele, que causam desconforto constante.",
+  },
+  {
+    title: "Queda de Cabelo",
+    text: "Perda de cabelo excessiva, levando a preocupações estéticas e de saúde.",
+  },
+  {
+    title: "Câncer de Pele",
+    text: "Lesões suspeitas ou diagnosticadas que necessitam de avaliação e possível remoção cirúrgica.",
+  },
+  {
+    title: "Sinais e Verrugas",
+    text: "Crescimento de sinais ou verrugas que incomodam estética ou funcionalmente, e que podem precisar de remoção.",
+  },
+  {
+    title: "Manchas na Pele",
+    text: "Descoloração da pele, como hiperpigmentação, que afeta a uniformidade do tom da pele.",
+  },
+  {
+    title: "Eczemas e Rashes",
+    text: "Erupções cutâneas, vermelhidão e irritação que causam desconforto e preocupação.",
+  },
+  {
+    title: "Cicatrizes",
+    text: "Cicatrizes de acne, cirurgias ou ferimentos que os pacientes desejam melhorar ou remover por razões estéticas.",
+  },
+];
+
+export const works = [
+  {
+    title: "Acne",
+    text: "A acne é uma condição dermatológica comum, especialmente durante a adolescência, marcada por espinhas, cravos e inflamações. Além de afetar a pele, pode impactar a autoestima. Conhecer suas causas e explorar tratamentos, como medicamentos e procedimentos estéticos, é essencial para um controle eficaz e duradouro.",
+    videoUrl: "https://www.youtube.com/embed/6ZJpuktJB1E?si=4ktl-LCNUPoZZRwj",
+  },
+  {
+    title: "Cirurgias Dermatologicas",
+    text: "As cirurgias dermatológicas tratam diversas condições da pele, como sinais, verrugas, cistos e até tumores malignos. Esses procedimentos, realizados por especialistas, são indicados tanto por motivos estéticos quanto funcionais, garantindo a saúde e a aparência da pele. Fatores como genética, exposição ao sol e histórico familiar podem influenciar essas condições.",
+    videoUrl: "https://www.youtube.com/embed/qMSt1WWy9jw?si=fuSDFVfHI_kSvwBy",
+  },
+  {
+    title: "Cancer de pele",
+    text: " O câncer de pele é o tipo mais comum de câncer, incluindo o carcinoma basocelular, o espinocelular e o melanoma. Pessoas com pele clara, predisposição genética ou exposição prolongada ao sol têm maior risco. Manchas ou feridas que não cicatrizam são sinais de alerta. O diagnóstico precoce é crucial, e a remoção cirúrgica pode salvar vidas. Conscientização sobre os sinais e proteção solar são fundamentais para a prevenção.",
+    videoUrl: "https://www.youtube.com/embed/crmwODVK_5g?si=QupVryrnUnWCNMI_",
+  },
+];
+
+export const questionsAndAnswer = [
+  {
+    title:
+      "Quais são as principais condições tratadas pelo Dr. Jorge Medeiros?",
+    answer:
+      "Dr. Jorge Medeiros é especializado no tratamento de diversas condições dermatológicas, incluindo acne, psoríase, melasma, dermatite atópica, além de realizar cirurgias dermatológicas para remoção de sinais e tratamento de câncer de pele.",
+  },
+  {
+    title: "Como é realizado o tratamento para acne?",
+    answer:
+      "O tratamento da acne é personalizado de acordo com a gravidade e tipo da condição. Pode incluir medicamentos tópicos, orais, tratamentos a laser, e procedimentos estéticos. A consulta inicial é fundamental para determinar o melhor plano de tratamento.",
+  },
+  {
+    title: "O Dr. Jorge Medeiros realiza cirurgia dermatológica?",
+    answer:
+      "Sim, Dr. Jorge Medeiros realiza cirurgias dermatológicas para uma variedade de condições, incluindo remoção de sinais, verrugas, cistos, e tratamento de câncer de pele. Esses procedimentos são feitos de forma segura e com foco na saúde e estética do paciente.",
+  },
+  {
+    title: "O Dr. Jorge Medeiros atende por plano de saúde?",
+    answer:
+      "Dr. Jorge Medeiros atende apenas pelo plano de saúde Hapvida. Para outros planos ou pacientes sem cobertura, os atendimentos são realizados de forma particular.",
+  },
+  {
+    title: "Como posso agendar uma consulta com o Dr. Jorge Medeiros?",
+    answer:
+      "Para agendar uma consulta, você pode entrar em contato diretamente com a clínica onde o Dr. Jorge Medeiros atende em Sobral. Os horários e disponibilidade serão ajustados de acordo com sua necessidade.",
+  },
+  {
+    title:
+      "Quais são os cuidados pós-operatórios após uma cirurgia dermatológica?",
+    answer:
+      "Os cuidados pós-operatórios variam de acordo com o procedimento realizado. Em geral, é importante seguir todas as orientações do Dr. Jorge Medeiros, que podem incluir o uso de medicamentos, evitar a exposição ao sol, e manter a área cirúrgica limpa e protegida Uma consulta de retorno será agendada para acompanhar a recuperação.",
+  },
+];

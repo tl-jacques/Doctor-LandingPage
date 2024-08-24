1cf2f2025522416b7c4c88d8e387f153a33602bf
Thiago Jacques <thiagolinhaes9@gmail.com>
2024-08-24 09:40:23 -0300

    page.tsx: adicionada sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import Complains from "@/sections/Complains";
+import Curriculum from "@/sections/Curriculum";
+import FAQ from "@/sections/FAQ";
 import Header from "@/sections/Header";
+import Presentation from "@/sections/Presentation";
+import Work from "@/sections/Work";
 
 export default function Home() {
   return (
     <main className="">
       <div className="">
         <Header />
+        <Presentation />
+        <Complains />
+        <Work />
+        <Curriculum />
+        <FAQ />
       </div>
     </main>
   );

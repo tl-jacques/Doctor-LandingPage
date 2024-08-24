2846a3bacb4e4ec2ced62718af9c2dd5c9b4de13
Thiago Jacques <thiagolinhaes9@gmail.com>
2024-08-24 09:38:57 -0300

    Presentation: Criada sessão e adicionada foto

---
+++ src/sections/Presentation.tsx
@@ -0,0 +1,46 @@
+const Presentation = () => {
+  return (
+    <section
+      className="scroll-smooth px-5 py-10 md:px-14 bg-[radial-gradient(ellipse_100%_100%_at_top_right,#34302B,#F3FBFE)]"
+      id="apresentacao"
+    >
+      <div className="flex flex-col mx-auto lg:max-w-[40rem] xl:max-w-[50rem] 2xl:max-w-[60rem]">
+        <div className="w-full py-3 px-2 ">
+          <div className="rounded-md w-full shadow-2xl backdrop-blur-2xl px-2 py-3">
+            <iframe
+              className="w-full h-[20rem] md:h-[23rem] lg:h-[26rem] xl:h-[30rem] 2xl:h-[40rem] "
+              src="https://www.youtube.com/embed/NkuijmYjP8Q?si=EjqKkCZVzhzYUdA9"
+              title="Apresentação dermatologista"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            />
+          </div>
+          <p className="leading-snug mt-6 font-medium xl:mt-8 xl:leading-normal ">
+            <span className="font-bold text-[1.05rem]">
+              Dr. Jorge Medeiros{" "}
+            </span>
+            é um dermatologista referência em Sobral,{" "}
+            <span className="font-bold">
+              especializado no tratamento de acne, psoríase, melasma e dermatite
+              atópica,
+            </span>{" "}
+            oferecendo cuidados personalizados para melhorar a saúde da pele.
+            Além disso, ele realiza{" "}
+            <span className="font-bold">
+              cirurgias dermatológicas para câncer de pele e remoção de sinais
+            </span>
+            , sempre focando na segurança e na saúde do seu paciente. Dr. Jorge
+            Medeiros combina conhecimento técnico e atenção individualizada para
+            proporcionar uma pele mais saudável aos seus pacientes.
+          </p>
+        </div>
+        <button className="w-full mt-4 btn bg-color-6 btn-primary flex items-center justify-center gap-4 shadow-md transition-[200ms] xl:mt-8 hover:cursor-pointer hover:opacity-90 hover:scale-105">
+          AGENDAR CONSULTA DERMATOLÓGICA
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Presentation;

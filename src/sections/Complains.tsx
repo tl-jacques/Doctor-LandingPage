7f23e576183bad39484ee438d9bc8d7dcfb74327
Thiago Jacques <thiagolinhaes9@gmail.com>
2024-08-24 09:40:08 -0300

    Complains: Criada section, adicionado componente Dropdown

---
+++ src/sections/Complains.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { mainComplains } from "@/constants/index";
+import Image from "next/image";
+import { IoIosArrowDown } from "react-icons/io";
+import DropDown from "@/components/DropDown";
+
+const Complains = () => {
+  return (
+    <section
+      className="baseSection flex flex-col mx-auto items-center text-center"
+      id="principais-queixas"
+    >
+      <h1 className="heading1 text-color-6">
+        Principais queixas e reclamações
+      </h1>
+      <ul className="grid grid-cols-2 md:grid-cols-3 gap-x-5 gap-y-7 py-10 2xl:gap-x-12 ">
+        {mainComplains.map((item, index) => (
+          <DropDown title={item.title} text={item.text} key={index} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Complains;

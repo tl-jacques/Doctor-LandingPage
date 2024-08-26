93e7734bdc96b91b496698c9929d4b5338b3697b
Thiago Jacques <thiagolinhaes9@gmail.com>
2024-08-26 15:43:14 -0300

    page.tsx: adicionada sessões do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import WhatsappButton from "@/components/WhatsappButton";
 import Complains from "@/sections/Complains";
 import Curriculum from "@/sections/Curriculum";
 import FAQ from "@/sections/FAQ";
+import Footer from "@/sections/Footer";
 import Header from "@/sections/Header";
 import Presentation from "@/sections/Presentation";
 import Work from "@/sections/Work";
@@ -8,13 +10,15 @@ import Work from "@/sections/Work";
 export default function Home() {
   return (
     <main className="">
-      <div className="">
+      <div className="relative">
         <Header />
         <Presentation />
         <Complains />
         <Work />
         <Curriculum />
         <FAQ />
+        <Footer />
+        <WhatsappButton />
       </div>
     </main>
   );
